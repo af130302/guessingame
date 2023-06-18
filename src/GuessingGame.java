29e48d6fa2dbbccbbe3b778f303b1a60d0825f17
Antoine Fonjallaz <antoine.fonjallaz@bluewin.ch>
2023-06-18 22:23:41 +0200

    Ajout d'une limite du nombre de tentatives et possibilité de jouer une autre partie.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -9,26 +9,42 @@ import java.util.Scanner;
 public class GuessingGame {
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 100;
+    private static final int MAX_ATTEMPTS = 5;
 
     public static void main(String[] args) {
-        int computerNumber = (int) (Math.random() * MAX_VALUE + MIN_VALUE);
-        int userAnswer = 0;
-        System.out.println("The correct guess would be " + computerNumber);
-        int count = 1;
         Scanner scanner = new Scanner(System.in);
+        boolean playAgain;
 
         do {
-            System.out.println("Enter a guess between " + MIN_VALUE + " and " + MAX_VALUE + ": ");
+            playGame(scanner);
+            System.out.println("Do you want to play again? (yes/no)");
+            String playAgainResponse = scanner.nextLine().toLowerCase();
+            playAgain = playAgainResponse.equals("yes");
+        } while (playAgain);
+    }
+
+    public static void playGame(Scanner scanner) {
+        int computerNumber = (int) (Math.random() * MAX_VALUE + MIN_VALUE);
+        int userAnswer;
+        System.out.println("The correct guess would be " + computerNumber);
+        int count = 1;
+
+        for (int attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
+            System.out.println("Attempt " + attempt + ": Enter a guess between " + MIN_VALUE + " and " + MAX_VALUE + ": ");
             String response = scanner.nextLine();
 
             try {
                 userAnswer = Integer.parseInt(response);
                 System.out.println(determineGuess(userAnswer, computerNumber, count));
                 count++;
+                if (userAnswer == computerNumber) {
+                    break;
+                }
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a valid integer.");
+                attempt--;
             }
-        } while (userAnswer != computerNumber);
+        }
     }
 
     public static String determineGuess(int userAnswer, int computerNumber, int count) {
@@ -37,9 +53,9 @@ public class GuessingGame {
         } else if (userAnswer == computerNumber) {
             return "Correct!\nTotal Guesses: " + count;
         } else if (userAnswer > computerNumber) {
-            return "Your guess is too high, try again.\nTry Number: " + count;
+            return "Your guess is too high, try again.";
         } else {
-            return "Your guess is too low, try again.\nTry Number: " + count;
+            return "Your guess is too low, try again.";
         }
     }
 }
