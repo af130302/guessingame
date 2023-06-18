8db1a3ff5d971c64d63c967d1bfc6b906b2c42cd
Antoine Fonjallaz <antoine.fonjallaz@bluewin.ch>
2023-06-18 22:20:23 +0200

    Ajout de la gestion des erreurs pour une saisie non entière de l'utilisateur et utilisation de constantes pour les valeurs minimale et maximale.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -4,33 +4,42 @@ Java game "Guess a Number" that allows user to guess a random number that has be
 source: https://hackr.io/blog/java-projects
 */
 
+import java.util.Scanner;
+
 public class GuessingGame {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
+
     public static void main(String[] args) {
-        int computerNumber = (int) (Math.random() * 100 + 1);
+        int computerNumber = (int) (Math.random() * MAX_VALUE + MIN_VALUE);
         int userAnswer = 0;
         System.out.println("The correct guess would be " + computerNumber);
         int count = 1;
+        Scanner scanner = new Scanner(System.in);
 
-        while (userAnswer != computerNumber) {
-            System.out.println("Enter a guess between 1 and 100: ");
-            String response = System.console().readLine();
-            userAnswer = Integer.parseInt(response);
-            System.out.println(determineGuess(userAnswer, computerNumber, count));
-            count++;
-        }
+        do {
+            System.out.println("Enter a guess between " + MIN_VALUE + " and " + MAX_VALUE + ": ");
+            String response = scanner.nextLine();
+
+            try {
+                userAnswer = Integer.parseInt(response);
+                System.out.println(determineGuess(userAnswer, computerNumber, count));
+                count++;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        } while (userAnswer != computerNumber);
     }
 
     public static String determineGuess(int userAnswer, int computerNumber, int count) {
-        if (userAnswer <= 0 || userAnswer > 100) {
+        if (userAnswer < MIN_VALUE || userAnswer > MAX_VALUE) {
             return "Your guess is invalid";
         } else if (userAnswer == computerNumber) {
             return "Correct!\nTotal Guesses: " + count;
         } else if (userAnswer > computerNumber) {
             return "Your guess is too high, try again.\nTry Number: " + count;
-        } else if (userAnswer < computerNumber) {
-            return "Your guess is too low, try again.\nTry Number: " + count;
         } else {
-            return "Your guess is incorrect\nTry Number: " + count;
+            return "Your guess is too low, try again.\nTry Number: " + count;
         }
     }
 }
